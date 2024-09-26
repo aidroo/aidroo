2f18d95aaddfb28d1b1b284f5a9f1481de9847e5
aidroo <aidrooteam@gmail.com>
2024-09-26 21:51:05 +0600

    fff

--- src/app/page.js
+++ src/app/page.js
@@ -16,3 +16,38 @@ export default function Home() {
     </Layout>
   );
 }
+// import { getAllBusinessProfile } from "../queries/admin-dashboard-getProfiles";
+
+// export default async function sitemap() {
+//   try {
+//     // Fetch all business profiles
+//     const { businessProfiles } = await getAllBusinessProfile();
+
+//     // Create an array of dynamic business URLs for the sitemap
+//     const business = businessProfiles.map((profile) => ({
+//       url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/business/${profile.username}`,
+//     }));
+
+//     // Return the array of URLs, including static ones
+//     return [
+//       {
+//         url: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
+//         lastModified: new Date().toISOString(),
+//       },
+//       ...business,
+//     ];
+//   } catch (error) {
+//     console.error("Error generating sitemap:", error);
+//     return [];
+//   }
+// }
+// export default function robots() {
+//   return {
+//     rules: {
+//       userAgent: "*",
+//       allow: "/",
+//       disallow: "/admin*",
+//     },
+//     sitemap: `${process.env.NEXT_PUBLIC_API_BASE_URL}/sitemap.xml`,
+//   };
+// }
